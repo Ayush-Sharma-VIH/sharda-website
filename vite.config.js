4828cdfe2682c04b65acbce09bb5dcaf38960740
Ayush-Sharma-2023 <Ayushsharmaarps@gmail.com>
2024-10-23 09:08:37 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/sharda-edu-website/', // Your repository name
+  base: '/sharda-website/', // Your repository name
 });
