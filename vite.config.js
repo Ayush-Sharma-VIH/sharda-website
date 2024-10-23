5bf2ba157f42742d51a4e3522b25c30a268d5cae
Ayush-Sharma-2023 <Ayushsharmaarps@gmail.com>
2024-10-23 09:13:00 +0530

    ss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';  // Make sure this plugin is imported
 
 export default defineConfig({
   base: '/sharda-website/', // Your repository name
+  plugins: [react()],       // Use the React plugin here to enable automatic JSX runtime
 });
