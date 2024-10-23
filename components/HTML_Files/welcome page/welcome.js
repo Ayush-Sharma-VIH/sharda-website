3f6532d2e8771d29e9f0bae86b3deb2f2ba85fd7
RoamingTrooper <altafraza7206@gmail.com>
2024-10-23 09:32:39 +0530

    new files(welcome page, contact page , about page) added

---
+++ components/HTML_Files/welcome page/welcome.js
@@ -0,0 +1,10 @@
+
+window.onload = () => {
+    const content = document.querySelector('.welcome-content');
+    const image = document.querySelector('.image-section img');
+
+    
+    setTimeout(() => {
+        image.classList.add('hover-animate');
+    }, 800);
+};
